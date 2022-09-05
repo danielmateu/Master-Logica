1c59ce0b90d03d26ee6994fd45d41f557d0d24d5
Daniel Mateu <danielmateu86@gmail.com>
2022-09-05 14:31:40 +0200

    Version con bucle for of

--- ejercicio-16/ejercicio-16.js
+++ ejercicio-16/ejercicio-16.js
@@ -8,4 +8,19 @@ const vocales = (myString) => {
 }
 
 console.log(vocales('daniel mateu pardo'));
+
+//Solución con ciclo for
+
+const vocalesFor = (myString) => {
+    let counter = 0;
+
+    let dividedString = myString.toLowerCase().split('');
+
+    for(let word of dividedString){
+        if(word === 'a' || word === 'e' || word === 'i' || word === 'o' ||word === 'u') counter++;    
+    }
+    return `La frase tiene ${counter} vocales`
+}
+
+console.log(vocalesFor('daniel mateu pardo')) 
     
