59089d9147db31c5afdd9a17c30df390074a7992
Daniel Mateu <danielmateu86@gmail.com>
2022-09-06 13:51:45 +0200

    Ejercicio 19 solucionado

--- ejercicio-19/ejercicio-19.js
+++ ejercicio-19/ejercicio-19.js
@@ -1,2 +1,41 @@
 
+const coleccionDePeliculas = [
+    { 
+        name: 'El señor de los anillos',
+        director: 'Peter Jackson',
+        vista: true
+    },
+    { 
+        name: 'La liga de la justicia',
+        director: 'Zack Snider',
+        vista: false
+    },
+    { 
+        name: 'El Hobbit',
+        director: 'Peter Jackson',
+        vista: true
+    },
+    { 
+        name: 'La cosa',
+        director: 'Zack Snider',
+        vista: false
+    },
+];
+
+
+const misPeliculas = () => {
+    for(let pelicula of coleccionDePeliculas){
+        let paraMostrar = `${pelicula.name} de ${pelicula.director}`;
+
+        if(pelicula.vista === true){
+            console.log(`Ya has visto ${paraMostrar}`);
+        }else{
+            console.log(`Te falta por ver ${paraMostrar}`);
+        }
+    }
+
+    return coleccionDePeliculas
+}
+console.log(misPeliculas());
+    
 
