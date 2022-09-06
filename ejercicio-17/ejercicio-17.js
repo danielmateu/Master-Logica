a4edfcd7e136171dbfa9510dad18e66914544cb3
Daniel Mateu <danielmateu86@gmail.com>
2022-09-06 12:37:48 +0200

    Version sin imprimir la lista total de numeros

--- ejercicio-17/ejercicio-17.js
+++ ejercicio-17/ejercicio-17.js
@@ -30,3 +30,19 @@ const imprimir = (miNumero) => {
 }
 
 console.log(imprimir(20));
+
+//Solución sin imprimir la lista de numeros 
+
+const buzzLightyear = (miNumero) => {
+    for (let i = 1; i < miNumero; i++){
+        if(i % 15 === 0){
+            console.log(i,'BuzzLightyear');
+        }else if(i % 3 === 0){
+            console.log(i,'Buzz');
+        }else if(i % 5 === 0){
+            console.log(i,'Lightyear');
+        }
+    }
+}
+
+buzzLightyear(20);
